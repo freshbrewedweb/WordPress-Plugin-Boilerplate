09ad251d0266e2de38895dd5d86873ce9fa3c8a6
Greg Hunt <freshbrewedweb@gmail.com>
2016-06-15 13:17:31 -0400

    Process IPN done

--- includes/class-project-donations-process.php
+++ includes/class-project-donations-process.php
@@ -89,41 +89,36 @@ class Project_Donations_Process_Donation
 
     $errors = array();
     $listener = new IpnListener();
+
     $logTime = date('Y-m-d H:i:s', time());
 
     if( $this->option('sandbox') == "on" )
       $listener->use_sandbox = true;
 
-    // 1. Check that $_POST['payment_status'] is "Completed"
     if( isset($_POST['payment_status']) && $_POST['payment_status'] !== "Completed" )
       $errors[$logTime][] = "Payment not completed.";
 
-    // 2. Check that $_POST['txn_id'] has not been previously processed
-    if( isset($_POST['txn_id']) && count($db->donations("transaction_id = ?", $_POST['txn_id'])) > 0 )
-      $errors[$logTime][] = "Transaction ID {$_POST['txn_id']} already exists.";
-
-    // 3. Check that $_POST['receiver_email'] is your Primary PayPal email
-    if( isset($_POST['receiver_email']) && $_POST['receiver_email'] !== PAYPAL_ACCOUNT_EMAIL )
-      $errors[$logTime][] = "Source ({$_POST['receiver_email']}) is not " . PAYPAL_ACCOUNT_EMAIL;
-
-    // 4. Check that $_POST['payment_amount'] and $_POST['payment_currency'] are correct
-    // TO DO
+    if( isset($_POST['receiver_email']) && $_POST['receiver_email'] !== $this->option('paypal_email') )
+      $errors[$logTime][] = "Source ({$_POST['receiver_email']}) is not " . $this->option('paypal_email');
 
     // Valid IPN
-    //
-    return $_POST;
-
-    if ( empty($errors) && $verified = $listener->processIpn()) {
+    if ( empty($errors) && $verified = $listener->processIpn() && isset($_POST['txn_id']) ) {
 
       $transactionRawData = $listener->getRawPostData();      // raw data from PHP input stream
       $transactionData = $listener->getPostData();            // POST data array
+	  $content = array();
+	  foreach( $transactionData as $data ) {
+		  $item = explode("=", $data);
+		  $content[$item[0]] = $item[1];
+	  }
+	  $post = wp_insert_post( array(
+	    'post_title' => $_POST['txn_id'] . ' – ' . $_POST['payer_email'],
+	    'post_content' => json_encode($content),
+	    'post_type' => 'donations',
+	    'post_status' => 'publish'
+	  ) );
 
-      $post = wp_insert_post( array(
-        'post_title' => $_POST['txn_id'],
-        'post_content' => json_encode($transactionData),
-      ) );
-
-      if( $post && $_POST['payment_status'] == "Completed" ) {
+      if( $post ) {
 
         // //Add to mailchimp list
         // $mail = new Mailchimp('9c4d1330f441bd2bdf5b0e496e4a4425-us9');
@@ -145,21 +140,21 @@ class Project_Donations_Process_Donation
         return TRUE;
 
       } else {
-        $errors[$logtime][] = "Failed to save donation (Transaction ID: {$result['txn_id']}).";
-        return FALSE;
+        $errors[$logtime][] = "Failed to save donation (Transaction ID: {$_POST['txn_id']}).";
       }
 
 
     } else {
         // Invalid IPN
-        $errors[$logTime][] = $listener->getErrors();
-        // Feel free to modify path and filename. Make SURE THE DIRECTORY IS WRITEABLE!
-        // For security reasons, you should use a path above/outside of your webroot
+        $errors[$logTime][] = "Invalid IPN: " . $listener->getErrors();
     }
 
-    if( count($errors) > 0 )
+    if( count($errors) > 0 ) {
       file_put_contents(plugin_dir_path( __FILE__ ) . 'logs/errors.log', print_r($errors, TRUE) . PHP_EOL, LOCK_EX | FILE_APPEND);
 
+	  return $errors;
+
+    }
   }
 
   /**
